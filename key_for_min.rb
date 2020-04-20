4aa2b6c1c7827b9ce1edd1ba9110af5d3c90c697
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-20 01:48:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect{|key| key[:adam] < name_hash[:adam]}
+  name_hash.find{|key| key[:adam] < name_hash[:adam]}
 end
