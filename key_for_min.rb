700a891b9bff465d4461388786c7c04577e542e3
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-20 05:24:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = 0
-  name_hash.each do |name, value|
+  name_hash.map do |name, value|
     if min_value < value
       min_value = value
       min_key = name
