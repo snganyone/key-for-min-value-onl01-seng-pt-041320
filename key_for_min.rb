874b362414c85014775787d6b3abac8d93639693
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-20 05:31:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = 0
+  min_value = Float::INFINITY
   name_hash.each do |name, value|
     if value <=> min_value
       min_value = value
